0e88231d8683a40c9a3f75feb9ba54c4c7e94e5f
lamanоnim <lamaanmustafayeva0@gmail.com>
2026-05-02 16:44:24 +0000

    Add 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square."""
+        self.__size = size
